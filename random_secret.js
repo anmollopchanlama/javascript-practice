16f4f537d5f3c1f23c7d6d29b5db9d28805f88f9
Anmol Lama <anmollopchanlama@gmail.com>
2026-08-18 00:22:06 +0545

    random secrte math random used

---
+++ random_secret.js
@@ -0,0 +1,12 @@
+// Use Math.random() to generate a decimal.If the number is less 
+// than 0.5, log "Flag 1: Secret Key Unlocked". Otherwise, log
+//  "Flag 2: Regular Access Only".
+
+let flag1, flag2;
+
+if (Math.random()<0.5){
+    console.log(flag1 = "Flag 1: secret key unlocked");
+}
+else{
+    console.log(flag2 = "Flag 2: regular access only ");
+}
