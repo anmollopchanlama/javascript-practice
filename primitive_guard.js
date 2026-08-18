3d67db327dd02866c7accaf9bae7c2265cd21e39
Anmol Lama <anmollopchanlama@gmail.com>
2026-08-18 15:59:21 +0545

    booleans

---
+++ primitive_guard.js
@@ -0,0 +1,14 @@
+// Create a variable named isPremium and set it to true.Use an if
+//  statement with the strict equality operator (===) to check if 
+//  isPremium is exactly equal to true.If it is, log 
+//  "Access Granted". 
+// Otherwise, log "Access Denied".
+
+let isPremium = true
+
+if (isPremium === true ){
+    console.log("Access granted")
+}
+else{
+    console.log("Access Denied")
+}
