e2c4bf6cfcd7934a9ffea6985fea1ac143a51ab4
Anmol Lama <anmollopchanlama@gmail.com>
2026-08-18 23:51:31 +0545

    do_while used

---
+++ do_while_loop.js
@@ -0,0 +1,10 @@
+// Create a variable named x and set it to 10.Create a do while 
+// loop.Inside the loop, print "Running once" using console.log().
+// Set the condition at the bottom to run while x < 5.
+
+let x = 10;
+
+do{
+    console.log("Running once");
+}
+while (x <5 );
