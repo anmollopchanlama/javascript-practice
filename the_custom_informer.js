377c26ff0fc1e5125429ac59bfa7e3e186f829a1
Anmol Lama <anmollopchanlama@gmail.com>
2026-08-19 12:02:58 +0545

    work with funcitons

---
+++ the_custom_informer.js
@@ -0,0 +1,12 @@
+// // Write a function named informUser.Inside the function, print
+//  the text "Target Exploited".Call the function so it runs.
+
+
+
+function informUser (){
+    
+    console.log("Target Exploited");
+
+}
+
+informUser();
