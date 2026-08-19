cd96006badb58eeaeb8c1fc093765e8159f1d003
Anmol Lama <anmollopchanlama@gmail.com>
2026-08-19 12:21:02 +0545

    add scanTarget function with multiple arguemetns for mulit-parameter logic

---
+++ multiple_parameter_scan.js
@@ -0,0 +1,12 @@
+// Write a function named scanTarget.Give it two parameters: 
+// ip and port.Inside the function, print them together like 
+// this: console.log("Scanning " + ip + " on port " + port);
+// Call the function at the bottom, passing the string 
+// "192.168.1.1" and the number 80 into it.
+
+function scanTarget( ip ,  port) {
+    console.log("Scanning " + ip + "on port " + port) 
+
+}
+
+scanTarget('192.168.1.1' ,80);
